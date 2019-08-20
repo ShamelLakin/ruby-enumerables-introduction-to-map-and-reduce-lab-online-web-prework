6ec8317216ab75a2f8952b2db671a1e49dacf552
Shamel Lakin <christlife4u@gmail.com>
2019-08-20 15:14:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,6 @@
 map_to_negativize [ 1, 2, 3, -9 ]
 p map_to_negativize.map { |i| i * -1 }
 
-map_to_no_change [1, 2, 3]
+dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+map_to_no_change [ "dune"]
 p map_to_no_change.map{1, 2, 3}
