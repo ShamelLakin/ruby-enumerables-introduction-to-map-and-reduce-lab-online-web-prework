c422413e45ccda04fd37422fb62fea00ea278dd5
Shamel Lakin <christlife4u@gmail.com>
2019-08-20 15:38:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,7 @@
 def map_to_negativize [ 1, 2, 3, -9 ]
  map_to_negativize.map { |i| i * -1 }
 end
+p map_to_negativize ([ 1, 2, 3, -9 ])
 
 def dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
 map_to_no_change ([dune])
@@ -15,7 +16,7 @@ p map_to_double ([1, 2, 3, -9])
 def map_to_square([1, 2, 3, -9])
   map_to_square.map { |n| n ** 2}
 end
-p map_to_square 
+p map_to_square([1, 2, 3, -9])
 
 def reduce_to_total([1, 2, 3,])
   reduce_to_totoal.reduce(0) { |sum, num| sum + num }
