e61dd022d6f25f7d8a1baa8d6cca30aa0317091f
Shamel Lakin <christlife4u@gmail.com>
2019-08-20 15:22:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ p map_to_negativize.map { |i| i * -1 }
 
 dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
 map_to_no_change ([dune])
-p map_to_no_change.map(dune)
+p map_to_no_change.map{dune}
 
 map_to_double([1, 2, 3, -9])
 p map_to_double.map { |n| n * 2}
